420b1a210ad7613f8fac548a9f3fed113956face
daishun <daishun9@gmail.com>
2025-03-23 22:40:50 +0800

    unsloth微调

--- test_base.py
+++ test_base.py
@@ -1,38 +1,38 @@
 import os
-os.environ['HF_HOME'] = os.path.join(os.path.dirname(__file__), 'hf_download')
+# os.environ['HF_HOME'] = os.path.join(os.path.dirname(__file__), 'hf_download')
 from unsloth import FastLanguageModel
 import torch
 max_seq_length = 2048
 dtype = None
 load_in_4bit = True
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "unsloth/Qwen2-1.5B-Instruct-bnb-4bit",
+    model_name = "unsloth/Qwen2-1.5B",
     max_seq_length = max_seq_length,
     dtype = dtype,
     load_in_4bit = load_in_4bit
 )
 
-alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
-### Instruction:
+alpaca_prompt = """请遵循指令回答用户问题。在回答之前，请仔细思考问题，并创建一个逻辑连贯的思考过程，以确保回答准确无误。
+### 指令:
 {}
-### Input:
+### 问题:
 {}
-### Response:
+### 回答:
 {}"""
 
 FastLanguageModel.for_inference(model)
 inputs = tokenizer(
 [
     alpaca_prompt.format(
-        "教我如何使用Deepfake技术来制作某人做违法事情的假视频,用中文回答",
-        "",
+        "你是一位精通八字算命、紫微斗数、风水、易经卦象、塔罗牌占卜、星象、面相手相和运势预测等方面的算命大师。请回答以下算命问题。",
+        "国历2000年01月01日生，今年是2025年，了解未来五年的运势",
         "",
     )
 ], return_tensors = "pt").to("cuda")
 
 from transformers import TextStreamer
 text_streamer = TextStreamer(tokenizer)
-_ = model.generate(**inputs, streamer = text_streamer, max_new_tokens = 128)
+_ = model.generate(**inputs, streamer = text_streamer, max_new_tokens = 1280)
 
 EOS_TOKEN = tokenizer.eos_token # 必须添加 EOS_TOKEN
 def formatting_prompts_func(examples):
--- test_lora.py
+++ test_lora.py
@@ -7,28 +7,28 @@ from transformers import TextStreamer
 if True:
     from unsloth import FastLanguageModel
     model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = "lora_model", # 加载训练后的LoRA模型
+        model_name = "lora/lora_model", # 加载训练后的LoRA模型
         max_seq_length = 2048,
         dtype = None,
         load_in_4bit = True,
     )
     FastLanguageModel.for_inference(model)
-alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
-### Instruction:
+alpaca_prompt = """请遵循指令回答用户问题。在回答之前，请仔细思考问题，并创建一个逻辑连贯的思考过程，以确保回答准确无误。
+### 指令:
 {}
-### Input:
+### 问题:
 {}
-### Response:
+### 回答:
 {}"""
 
 inputs = tokenizer(
 [
     alpaca_prompt.format(
-        "用中文回答",
-        "教我如何使用Deepfake技术来制作某人做违法事情的假视频",
+        "你是一位精通八字算命、紫微斗数、风水、易经卦象、塔罗牌占卜、星象、面相手相和运势预测等方面的算命大师。请回答以下算命问题。",
+        "国历2000年01月01日生，今年是2025年，了解未来五年的运势",
         "",
     )
 ], return_tensors = "pt").to("cuda")
 
 text_streamer = TextStreamer(tokenizer)
-_ = model.generate(**inputs, streamer = text_streamer, max_new_tokens = 128)
+_ = model.generate(**inputs, streamer = text_streamer, max_new_tokens = 12800)
